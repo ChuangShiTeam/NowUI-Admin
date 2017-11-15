38fc04612333f228e1c62cf4f0bd30cc036d5bf4
marcus <marcus_hualebin@163.com>
2017-11-15 21:41:41 +0800

    完善

--- src/common/constant.js
+++ src/common/constant.js
@@ -2,7 +2,8 @@ export default {
     // host: 'http://localhost:8080',
     host: 'http://api.chuangshi.nowui.com',
     image_host: 'http://api.chuangshi.nowui.com',
-    app_id: '749388e5dac3465f922c54e61d16a993',
+    app_id: 'fc62acaf870c45f3971f5aa7b3f09f69',
     platform: 'PC',
-    version: '1.0.0'
+    version: '1.0.0',
+    name: '创石--总控后台'
 };
