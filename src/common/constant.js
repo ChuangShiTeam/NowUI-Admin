23167018ac47ef3a9bc5e3acafac6e62e40f18be
marcus <marcus_hualebin@163.com>
2017-11-15 21:44:31 +0800

    完善

--- src/common/constant.js
+++ src/common/constant.js
@@ -5,5 +5,7 @@ export default {
     app_id: 'fc62acaf870c45f3971f5aa7b3f09f69',
     platform: 'PC',
     version: '1.0.0',
-    name: '创石--总控后台'
+    name: '创石--总控后台',
+    index: '/product/index',
+    login: '/admin/admin/login'
 };
