dec6c244873e23744e96af4ee2ead1cc5b32c12a
NowUI <nowui@163.com>
2017-11-23 17:43:08 +0800

    修改

--- src/Routers.js
+++ src/Routers.js
@@ -4,11 +4,6 @@ import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 import {routerReducer} from 'react-router-redux';
 
-// import dashboard from './store/dashboard';
-// import app_config from './store/app_config';
-// import app_config_category from './store/app_config_category';
-// import product from './store/product';
-
 const reducers = {};
 const context = require.context('./store', false, /\.js$/);
 const keys = context.keys().filter(item => item !== './index.js');
@@ -22,27 +17,8 @@ const store = createStore(
     }))
 );
 
-// const store = createStore(
-//     combineReducers({
-//         dashboard,
-//         app_config,
-//         app_config_category,
-//         product,
-//         routing: routerReducer
-//     })
-// );
-
 const handleEnter = function (next, replace, callback) {
     callback();
-};
-
-function handleLazyLoad(path, name) {
-    return function (location, cb) {
-        require.ensure([], (require) => {
-            console.log(path);
-            cb(null, require('' + path).default)
-        }, 'm');
-    }
 };
 
 const Routers = () =>
@@ -55,7 +31,11 @@ const Routers = () =>
                         cb(null, require('./view/Login').default)
                     }, 'login');
                 }}/>
-                <Route getComponent={handleLazyLoad('./view/Main', '')} onEnter={handleEnter}>
+                <Route getComponent={function (location, cb) {
+                    require.ensure([], (require) => {
+                        cb(null, require('./view/Main').default)
+                    }, 'm');
+                }} onEnter={handleEnter}>
                     <Route path="/dashboard/index" getComponent={function (location, cb) {
                         require.ensure([], (require) => {
                             cb(null, require('./view/dashboard/Index').default)
