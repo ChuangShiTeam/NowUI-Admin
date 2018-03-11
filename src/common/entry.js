d6a2dbefeb584e233633c570ff0b87666dbf94c3
marcus <marcus_hualebin@163.com>
2018-03-11 11:56:35 +0800

    模块动态加载配置完善

--- src/common/entry.js
+++ src/common/entry.js
@@ -1,111 +1,148 @@
 export default {
-	webEntry: [{
+	webEntry: [
+	//	{
 	// 	store: require.context('../store', false, /admin.js/),
 	// 	router: require.context('../router', false, /admin.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /advertisement.js/),
 	// 	router: require.context('../router', false, /advertisement.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /app.js/),
 	// 	router: require.context('../router', false, /app.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /appConfig.js/),
 	// 	router: require.context('../router', false, /appConfig.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /appConfigCategory.js/),
 	// 	router: require.context('../router', false, /appConfigCategory.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /article.js/),
 	// 	router: require.context('../router', false, /article.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /articleAudit.js/),
 	// 	router: require.context('../router', false, /articleAudit.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /articleAuthor.js/),
 	// 	router: require.context('../router', false, /articleAuthor.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /articleCategory.js/),
 	// 	router: require.context('../router', false, /articleCategory.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /articlePrice.js/),
 	// 	router: require.context('../router', false, /articlePrice.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /articleUserPurchase.js/),
 	// 	router: require.context('../router', false, /articleUserPurchase.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /code.js/),
 	// 	router: require.context('../router', false, /code.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /code.js/),
 	// 	router: require.context('../router', false, /code.js/)
-	// }, {
+	// },
+	{
 		store: require.context('../store', false, /dashboard.js/),
 		router: require.context('../router', false, /dashboard.js/)
-	// }, {
+	},
+	// {
 	// 	store: require.context('../store', false, /forum.js/),
 	// 	router: require.context('../router', false, /forum.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /forumAudit.js/),
 	// 	router: require.context('../router', false, /forumAudit.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /forumCategory.js/),
 	// 	router: require.context('../router', false, /forumCategory.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /forumUserFollow.js/),
 	// 	router: require.context('../router', false, /forumUserFollow.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /forumUserUnfollow.js/),
 	// 	router: require.context('../router', false, /forumUserUnfollow.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /member.js/),
 	// 	router: require.context('../router', false, /member.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /memberDefaultAvatar.js/),
 	// 	router: require.context('../router', false, /memberDefaultAvatar.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /memberDeliveryAddress.js/),
 	// 	router: require.context('../router', false, /memberDeliveryAddress.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /menu.js/),
 	// 	router: require.context('../router', false, /menu.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /message.js/),
 	// 	router: require.context('../router', false, /message.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /navigation.js/),
 	// 	router: require.context('../router', false, /navigation.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /patient.js/),
 	// 	router: require.context('../router', false, /patient.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /pet.js/),
 	// 	router: require.context('../router', false, /pet.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /petCategory.js/),
 	// 	router: require.context('../router', false, /petCategory.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /product.js/),
 	// 	router: require.context('../router', false, /product.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /purchaseOrder.js/),
 	// 	router: require.context('../router', false, /purchaseOrder.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /role.js/),
 	// 	router: require.context('../router', false, /role.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /supplier.js/),
 	// 	router: require.context('../router', false, /supplier.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /toolbar.js/),
 	// 	router: require.context('../router', false, /toolbar.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /topicCommentUserLike.js/),
 	// 	router: require.context('../router', false, /topicCommentUserLike.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /topicCommentUserUnlike.js/),
 	// 	router: require.context('../router', false, /topicCommentUserUnlike.js/)
-	// }, {
+	// },
+	// {
 	// 	store: require.context('../store', false, /topicForum.js/),
 	// 	router: require.context('../router', false, /topicForum.js/)
-	}]
+	// }
+	]
 };
